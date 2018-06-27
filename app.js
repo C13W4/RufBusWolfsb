1e23c05f3643d5b229e3a116136677d086948d54
C13W4 <c13.werner@gmail.com>
2018-06-27 15:24:28 +0200

    start-Form für Rufbus
    + install nodemon

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const port = 3000;
 
 fs.readFile('index.html', (err, html) => {
 
-	if(err){
-		throw err;
-	}
+	//if(err){
+	//	throw err;
+	//}
 
 		const server = http.createServer((req, res) => {
   		res.statusCode = 200;
@@ -23,4 +23,3 @@ fs.readFile('index.html', (err, html) => {
   		console.log('Server running at ' + hostname + ':'+ port);
 	});
 });
-
