b377f8ce1fda1240e5b5219e849f0f304fb2fadf
C13W4 <c13.werner@gmail.com>
2018-08-04 21:45:00 +0200

    Datenupload with MariaDB

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 //requirements...
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+        host: 'mycloud',
+        user: 'WVG_DB',
+        password: '8mA84UJ175md',
+        database: "WVG_DATA",
+        port: 3307
+});
 
 const http = require('http');
 const fs = require('fs');
 const { parse } = require('querystring');
-var path = require('path');
-
-var express = require('express');
-var app = express();
-var mysql = require('mysql');
-
 const hostname = 'localhost';
-
-const router = express.Router(); // 1
+const router = express.Router();
  
 
 
 // Konfiguration...
 app.use(express.static(__dirname + '/html'));
 
-//app.use(express.bodyParser());
-
-
-/*connection.query('USE test', function (err) {
+// Datenbankverbindung herstellen
+connection.connect(function(err) {
   if (err) throw err;
-  connection.query('CREATE TABLE IF NOT EXISTS users('
-        + 'id INT NOT NULL AUTO_INCREMENT,'
-            + 'PRIMARY KEY(id),'
-            + 'name VARCHAR(30)'
-            +  ')', function (err) {
-                if (err) throw err;
-            });
-});*/
+  console.log("\nConnected to mysql: WVG_DATA\n");
+});
 
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Aufruf-Händling
 app.get('/', function(req, res) {
@@ -44,22 +44,24 @@ app.get('/booking', function(req, res) {
 });
 
 
-app.post('/', function(req, res) {
+//Eingabe Daten per POST verarbeiten
+app.post('/', urlencodedParser,  function(req, res) {
   
-      var body = "";
+	console.log('\nName: ' + req.body.name);
+    console.log('Datum/Uhrzeit: ' + req.body.mydatetimepicker);
+    console.log('Abholung: ' + req.body.Haltestelle);
+    console.log('Ziel: ' + req.body.Ziel);
 
-      req.on('data', chunk => {
-          body += chunk.toString(); // convert Buffer to string
-      });
+	connection.query("INSERT INTO Buchungen(Name, Rufnummer, Passenger, Datum, Pickup, Target) VALUES ('"+req.body.name+"','"+req.body.Mobilnummer+"','"+req.body.Passagiere+"','"+req.body.mydatetimepicker+"','"+req.body.Haltestelle+"','"+req.body.Ziel+"')", function(err, result){
+    if(err) throw err;
+        
+        console.log("1 Datensatz eingefügt");
 
-      req.on('end', () => {
-          console.log(parse(body));
-          //res.end('ok');
-          res.end(body);
-      });
+        res.end(req.body.name);
+    });
 });
 
 
 //Server start
 app.listen(3000);
-console.log('Server running at Port localhost:3000');
+console.log('\nServer running at localhost:3000');
