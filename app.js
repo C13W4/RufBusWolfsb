78b0edc1cfb65d7446830dacd0ae3569f22b7a81
C13W4 <c13.werner@gmail.com>
2018-08-04 19:20:15 +0200

    static directory structure

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//requirements...
+
 const http = require('http');
 const fs = require('fs');
 const { parse } = require('querystring');
@@ -5,30 +7,40 @@ var path = require('path');
 
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 const hostname = 'localhost';
 
 const router = express.Router(); // 1
  
-app.listen(3000);
-
-console.log('Server running at Port localhost:3000');
 
 
+// Konfiguration...
+app.use(express.static(__dirname + '/html'));
+
+//app.use(express.bodyParser());
 
 
+/*connection.query('USE test', function (err) {
+  if (err) throw err;
+  connection.query('CREATE TABLE IF NOT EXISTS users('
+        + 'id INT NOT NULL AUTO_INCREMENT,'
+            + 'PRIMARY KEY(id),'
+            + 'name VARCHAR(30)'
+            +  ')', function (err) {
+                if (err) throw err;
+            });
+});*/
+
+
+// Aufruf-Händling
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/landing.html'));
-});
-
-
-app.get('/notizblock.jpg', function(req, res) {
-  res.sendFile(path.join(__dirname + '/notizblock.jpg'));
+  res.sendFile(path.join(__dirname + '/html/landing.html'));
 });
 
 
 app.get('/booking', function(req, res) {
-  res.sendFile(path.join(__dirname + '/booking.html'));
+  res.sendFile(path.join(__dirname + '/html/booking.html'));
 });
 
 
@@ -46,3 +58,8 @@ app.post('/', function(req, res) {
           res.end(body);
       });
 });
+
+
+//Server start
+app.listen(3000);
+console.log('Server running at Port localhost:3000');
